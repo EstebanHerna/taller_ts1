745b6d244bef2751a2239769ca6071a3c6f16c62
Esteban Alejandro Hernandez Sulvara <142023857+EstebanHerna@users.noreply.github.com>
2024-10-19 20:55:19 -0500

    Add files via upload

---
+++ data.ts
@@ -0,0 +1,9 @@
+
+export const series = [
+    { id: 1, title: "Breaking Bad", channel: "AMC", seasons: 5 },
+    { id: 2, title: "Orange Is the New Black", channel: "Netflix", seasons: 6 },
+    { id: 3, title: "Game of Thrones", channel: "HBO", seasons: 7 },
+    { id: 4, title: "The Big Bang Theory", channel: "CBS", seasons: 12 },
+    { id: 5, title: "Sherlock", channel: "BBC", seasons: 4 },
+    { id: 6, title: "A Very English Scandal", channel: "BBC", seasons: 2 }
+];
